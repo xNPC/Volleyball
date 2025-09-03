7b94d35913d585eba9b62b066a08e234faba316a
xNPC <mix-88@list.ru>
2025-09-03 22:07:33 +0700

    Документирование маршрутов

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -48,37 +48,37 @@ class PlatformProvider extends OrchidServiceProvider
                 ])
             ->route('platform.tournaments.list'),
 
-//            Menu::make('Get Started')
-//                ->icon('bs.book')
-//                ->title('Navigation')
-//                ->route(config('platform.index')),
-//
-//            Menu::make('Sample Screen')
-//                ->icon('bs.collection')
-//                ->route('platform.example')
-//                ->badge(fn () => 6),
-//
-//            Menu::make('Form Elements')
-//                ->icon('bs.card-list')
-//                ->route('platform.example.fields')
-//                ->active('*/examples/form/*'),
-//
-//            Menu::make('Layouts Overview')
-//                ->icon('bs.window-sidebar')
-//                ->route('platform.example.layouts'),
-//
-//            Menu::make('Grid System')
-//                ->icon('bs.columns-gap')
-//                ->route('platform.example.grid'),
-//
-//            Menu::make('Charts')
-//                ->icon('bs.bar-chart')
-//                ->route('platform.example.charts'),
-//
-//            Menu::make('Cards')
-//                ->icon('bs.card-text')
-//                ->route('platform.example.cards')
-//                ->divider(),
+            Menu::make('Get Started')
+                ->icon('bs.book')
+                ->title('Navigation')
+                ->route(config('platform.index')),
+
+            Menu::make('Sample Screen')
+                ->icon('bs.collection')
+                ->route('platform.example')
+                ->badge(fn () => 6),
+
+            Menu::make('Form Elements')
+                ->icon('bs.card-list')
+                ->route('platform.example.fields')
+                ->active('*/examples/form/*'),
+
+            Menu::make('Layouts Overview')
+                ->icon('bs.window-sidebar')
+                ->route('platform.example.layouts'),
+
+            Menu::make('Grid System')
+                ->icon('bs.columns-gap')
+                ->route('platform.example.grid'),
+
+            Menu::make('Charts')
+                ->icon('bs.bar-chart')
+                ->route('platform.example.charts'),
+
+            Menu::make('Cards')
+                ->icon('bs.card-text')
+                ->route('platform.example.cards')
+                ->divider(),
 
             Menu::make(__('Users'))
                 ->icon('bs.people')
