0c86c99f95a0028e6968e166a0f6c7c5522bdc5d
Andrey <mix-88@list.ru>
2025-11-18 15:05:57 +0700

    16 на странице

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,7 @@ class UserController extends Controller
                 $query->whereNotNull('email_verified_at');
             })
             ->orderBy('name')
-            ->paginate(2)
+            ->paginate(16)
             ->onEachSide(1)
             ->withQueryString();
 
